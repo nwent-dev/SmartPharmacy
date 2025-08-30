6d193458b78f9939481714e00206aa3ad02e2e8d
nwent <nwent@proton.me>
2025-08-30 21:59:05 +0300

    Initial Commit

---
+++ SmartPharmacy/SmartPharmacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPharmacyApp.swift
+//  SmartPharmacy
+//
+//  Created by Александр Калашников on 30/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartPharmacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
